4239c6c1afa0a624a90b79ab32e449bfc1b6c32e
cmorley <cmorley@62bbe328-0443-0410-860d-e6d70dd76ebd>
2000-03-03 21:49:04 +0000

    gcc 2.95.2 on solaris 2.6 wants a cast on the info accessor.

--- libvrml97core/src/vrml97/VrmlNodeWorldInfo.h
+++ libvrml97core/src/vrml97/VrmlNodeWorldInfo.h
@@ -1,84 +1,49 @@
 //
-
 //  Vrml 97 library
-
 //  Copyright (C) 1998 Chris Morley
-
+//
+//  VrmlNodeWorldInfo.h
 //
 
-//  %W% %G%
-
-//  VrmlNodeWorldInfo.h
-
-
-
 #ifndef  _VRMLNODEWORLDINFO_
-
 #define  _VRMLNODEWORLDINFO_
 
-
-
 #include "VrmlNodeChild.h"
-
 #include "VrmlMFString.h"
-
 #include "VrmlSFString.h"
-
-
-
 
 
 class VrmlNodeWorldInfo : public VrmlNodeChild {
 
-
-
 public:
 
-
-
   // Define the built in VrmlNodeType:: "WorldInfo"
-
   static VrmlNodeType *defineType(VrmlNodeType *t = 0);
 
   virtual VrmlNodeType *nodeType() const;
 
-
-
   VrmlNodeWorldInfo(VrmlScene *);
-
   virtual ~VrmlNodeWorldInfo();
-
 
 
   virtual VrmlNode *cloneMe() const;
 
-
-
   virtual ostream& printFields(ostream& os, int indent);
-
-
 
   virtual void setField(const char *fieldName, const VrmlField &fieldValue);
 
-
-
   const char* title() { return d_title.get(); }
 
-  const char** info() { return d_info.get(); }
+  const char** info() { return (const char**) (d_info.get()); }
 
   const int    size() { return d_info.size(); }
 
 
-
 private:
-
-
 
   VrmlMFString d_info;
 
   VrmlSFString d_title;
-
-  
 
 };
 
