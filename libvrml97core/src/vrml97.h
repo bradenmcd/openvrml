fc1be6230757fafd88f7949bad8c9dfda9e4ece0
cmorley <cmorley@62bbe328-0443-0410-860d-e6d70dd76ebd>
2000-06-29 13:17:14 +0000

    Include doc2, fix some typos.

--- libvrml97core/src/vrml97.h
+++ libvrml97core/src/vrml97.h
@@ -1,97 +1,112 @@
 #ifndef VRML97_H
 #define VRML97_H
 
-#include<vrml97/Audio.h>
-#include<vrml97/Doc.h>
-#include<vrml97/Image.h>
-#include<vrml97/ScriptJDK.h>
-#include<vrml97/ScriptJS.h>
-#include<vrml97/ScriptObject.h>
-#include<vrml97/System.h>
-#include<vrml97/Viewer.h>
-#include<vrml97/VrmlEvent.h>
-#include<vrml97/VrmlField.h>
-#include<vrml97/VrmlMFColor.h>
-#include<vrml97/VrmlMFFloat.h>
-#include<vrml97/VrmlMFInt.h>
-#include<vrml97/VrmlMFNode.h>
-#include<vrml97/VrmlMFRotation.h>
-#include<vrml97/VrmlMFString.h>
-#include<vrml97/VrmlMFVec2f.h>
-#include<vrml97/VrmlNamespace.h>
-#include<vrml97/VrmlNode.h>
-#include<vrml97/VrmlNodeAnchor.h>
-#include<vrml97/VrmlNodeAppearance.h>
-#include<vrml97/VrmlNodeAudioCluip.h>
-#include<vrml97/VrmlNodeBackground.h>
-#include<vrml97/VrmlNodeBillboard.h>
-#include<vrml97/VrmlNodeBindable.h>
-#include<vrml97/VrmlNodeBox.h>
-#include<vrml97/VrmlNodeChild.h>
-#include<vrml97/VrmlNodeCollision.h>
-#include<vrml97/VrmlNodeColor.h>
-#Include<vrml97/VrmlNodeColorInt.h>
-#include<vrml97/VrmlNodeCone.h>
-#include<vrml97/VrmlNodeCoordinate.h>
-#include<vrml97/VrmlNodeCoordinateInt.h>
-#include<vrml97/VrmlNodeCylinder.h>
-#include<vrml97/VrmlNodeCylinderSensor.h>
-#include<vrml97/VrmlNodeDirLight.h>
-#include<vrml97/VrmlNodeElevationGrid.h>
-#include<vrml97/VrmlNodeExtrusion.h>
-#include<vrml97/VrmlNodeFog.h>
-#include<vrml97/VrmlNodeFontStyle.h>
-#include<vrml97/VrmlNodeGeometry.h>
-#include<vrml97/VrmlNodeGroup.h>
-#include<vrml97/VrmlNodeIFaceSet.h>
-#include<vrml97/VrmlNodeILineSet.h>
-#include<vrml97/VrmlNodeImageTexture.h>
-#include<vrml97/VrmlNodeIndexedSet.h>
-#include<vrml97/VrmlNodeInline.h>
-#include<vrml97/VrmlNodeLOD.h>
-#include<vrml97/VrmlNodeLight.h>
-#include<vrml97/VrmlNodeMaterial.h>
-#include<vrml97/VrmlNodeMovieTexture.h>
-#include<vrml97/VrmlNodeNavigationInfo.h>
-#include<vrml97/VrmlNodeNormal.h>
-#include<vrml97/VrmlNodeNormalInt.h>
-#include<vrml97/VrmlNodeOrientationInt.h>
-#include<vrml97/VrmlNodePixelTexture.h>
-#include<vrml97/VrmlNodePlaneSensor.h>
-#include<vrml97/VrmlNodePointLight.h>
-#include<vrml97/VrmlNodePointSet.h>
-#include<vrml97/VrmlNodePositionInt.h>
-#include<vrml97/VrmlNodeProto.h>
-#include<vrml97/VrmlNodeProcimitySensor.h>
-#include<vrml97/VrmlNodeScalarInt.h>
-#include<vrml97/VrmlNodeScript.h>
-#include<vrml97/VrmlNodeShape.h>
-#include<vrml97/VrmlNodeSound.h>
-#include<vrml97/VrmlNodeSphere.h>
-#include<vrml97/VrmlNodeSphereSensor.h>
-#include<vrml97/VrmlNodeSpotLight.h>
-#include<vrml97/VrmlNodeSwitch.h>
-#include<vrml97/VrmlNodeText.h>
-#include<vrml97/VrmlNodeTexture.h>
-#include<vrml97/VrmlNodeTextureCoordinate.h>
-#include<vrml97/VrmlNodeTextureTransform.h>
-#include<vrml97/VrmlNodeTimeSensor.h>
-#include<vrml97/VrmlNodeTouchSensor.h>
-#include<vrml97/VrmlNodeTransform.h>
-#include<vrml97/VrmlNodeType.h>
-#include<vrml97/VrmlNodeViewpoint.h>
-#include<vrml97/VrmlNodeVisibilitySensor.h>
-#include<vrml97/VrmlNodeWorldInfo.h>
-#include<vrml97/VrmlSFBool.h>
-#include<vrml97/VrmlSFColor.h>
-#include<vrml97/VrmlSFImage.h>
-#include<vrml97/VrmlSFInt.h>
-#include<vrml97/VrmlSFNode.h>
-#include<vrml97/VrmlSFRotation.h>
-#include<vrml97/VrmlSFString.h>
-#include<vrml97/VrmlSFTime.h>
-#include<vrml97/VrmlSFVec2f.h>
-#include<vrml97/VrmlSFVec3f.h>
-#include<vrml97/VrmlScene.h>
+#include <vrml97/Audio.h>
+#include <vrml97/Doc.h>
+#include <vrml97/doc2.hpp>
+#include <vrml97/Image.h>
+#include <vrml97/MathUtils.h>
+#include <vrml97/ScriptJDK.h>
+#include <vrml97/ScriptJS.h>
+#include <vrml97/ScriptObject.h>
+#include <vrml97/System.h>
+#include <vrml97/Viewer.h>
+#include <vrml97/VrmlAABox.h>
+#include <vrml97/VrmlBSphere.h>
+#include <vrml97/VrmlBVolume.h>
+#include <vrml97/VrmlEvent.h>
+#include <vrml97/VrmlField.h>
+#include <vrml97/VrmlFrustum.h>
+#include <vrml97/VrmlMFColor.h>
+#include <vrml97/VrmlMFFloat.h>
+#include <vrml97/VrmlMFInt.h>
+#include <vrml97/VrmlMFNode.h>
+#include <vrml97/VrmlMFRotation.h>
+#include <vrml97/VrmlMFString.h>
+#include <vrml97/VrmlMFTime.h>
+#include <vrml97/VrmlMFVec2f.h>
+#include <vrml97/VrmlMFVec3f.h>
+#include <vrml97/VrmlNamespace.h>
+#include <vrml97/VrmlNode.h>
+#include <vrml97/VrmlNodeAnchor.h>
+#include <vrml97/VrmlNodeAppearance.h>
+#include <vrml97/VrmlNodeAudioClip.h>
+#include <vrml97/VrmlNodeBackground.h>
+#include <vrml97/VrmlNodeBillboard.h>
+#include <vrml97/VrmlNodeBox.h>
+#include <vrml97/VrmlNodeChild.h>
+#include <vrml97/VrmlNodeCollision.h>
+#include <vrml97/VrmlNodeColor.h>
+#include <vrml97/VrmlNodeColorInt.h>
+#include <vrml97/VrmlNodeCone.h>
+#include <vrml97/VrmlNodeCoordinate.h>
+#include <vrml97/VrmlNodeCoordinateInt.h>
+#include <vrml97/VrmlNodeCylinder.h>
+#include <vrml97/VrmlNodeCylinderSensor.h>
+#include <vrml97/VrmlNodeDirLight.h>
+#include <vrml97/VrmlNodeElevationGrid.h>
+#include <vrml97/VrmlNodeExtrusion.h>
+#include <vrml97/VrmlNodeFog.h>
+#include <vrml97/VrmlNodeFontStyle.h>
+#include <vrml97/VrmlNodeGeometry.h>
+#include <vrml97/VrmlNodeGroup.h>
+#include <vrml97/VrmlNodeIFaceSet.h>
+#include <vrml97/VrmlNodeILineSet.h>
+#include <vrml97/VrmlNodeImageTexture.h>
+#include <vrml97/VrmlNodeIndexedSet.h>
+#include <vrml97/VrmlNodeInline.h>
+#include <vrml97/VrmlNodeLOD.h>
+#include <vrml97/VrmlNodeLight.h>
+#include <vrml97/VrmlNodeMaterial.h>
+#include <vrml97/VrmlNodeMovieTexture.h>
+#include <vrml97/VrmlNodeNavigationInfo.h>
+#include <vrml97/VrmlNodeNormal.h>
+#include <vrml97/VrmlNodeNormalInt.h>
+#include <vrml97/VrmlNodeOrientationInt.h>
+#include <vrml97/VrmlNodePixelTexture.h>
+#include <vrml97/VrmlNodePlaneSensor.h>
+#include <vrml97/VrmlNodePointLight.h>
+#include <vrml97/VrmlNodePointSet.h>
+#include <vrml97/VrmlNodePositionInt.h>
+#include <vrml97/VrmlNodeProto.h>
+#include <vrml97/VrmlNodeProximitySensor.h>
+#include <vrml97/VrmlNodeScalarInt.h>
+#include <vrml97/VrmlNodeScript.h>
+#include <vrml97/VrmlNodeShape.h>
+#include <vrml97/VrmlNodeSound.h>
+#include <vrml97/VrmlNodeSphere.h>
+#include <vrml97/VrmlNodeSphereSensor.h>
+#include <vrml97/VrmlNodeSpotLight.h>
+#include <vrml97/VrmlNodeSwitch.h>
+#include <vrml97/VrmlNodeText.h>
+#include <vrml97/VrmlNodeTexture.h>
+#include <vrml97/VrmlNodeTextureCoordinate.h>
+#include <vrml97/VrmlNodeTextureTransform.h>
+#include <vrml97/VrmlNodeTimeSensor.h>
+#include <vrml97/VrmlNodeTouchSensor.h>
+#include <vrml97/VrmlNodeTransform.h>
+#include <vrml97/VrmlNodeType.h>
+#include <vrml97/VrmlNodeViewpoint.h>
+#include <vrml97/VrmlNodeVisibilitySensor.h>
+#include <vrml97/VrmlNodeWorldInfo.h>
+#include <vrml97/VrmlParse.h>
+#include <vrml97/VrmlRenderContext.h>
+#include <vrml97/VrmlSFBool.h>
+#include <vrml97/VrmlSFColor.h>
+#include <vrml97/VrmlSFFloat.h>
+#include <vrml97/VrmlSFImage.h>
+#include <vrml97/VrmlSFInt.h>
+#include <vrml97/VrmlSFNode.h>
+#include <vrml97/VrmlSFRotation.h>
+#include <vrml97/VrmlSFString.h>
+#include <vrml97/VrmlSFTime.h>
+#include <vrml97/VrmlSFVec2f.h>
+#include <vrml97/VrmlSFVec3f.h>
+#include <vrml97/VrmlScene.h>
+#include <vrml97/gifread.h>
+#include <vrml97/jpgread.h>
+#include <vrml97/mpgread.h>
+#include <vrml97/pngread.h>
+#include <vrml97/sound.h>
 
 #endif
