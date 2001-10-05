93e7dc4a73ac60ff6bf5b058718dfbb5a749137a
tflynn <tflynn@62bbe328-0443-0410-860d-e6d70dd76ebd>
2001-10-05 04:20:00 +0000

    Add javadoc comments.

--- java/vrml/Browser.java
+++ java/vrml/Browser.java
@@ -20,37 +20,130 @@
 
 package vrml;
 
+/*
+ * The browser interface provides a mechanism for scripts contained by 
+ * Script nodes to get and set browser state.
+ */
 public class Browser 
 {
+  /*
+   * Stores a pointer to a VrmlScene that will be used on the C++ side
+   * to map calls for any of the event of the class.
+   */
   private int BrowserPtr;
 
+  /*
+   * @see java.lang.Object
+   */
   public native String toString();
- 
-  // Browser interface
-  public native String getName();    
+
+  /*
+   * Name of the browser currently in use.
+   *
+   * @return name of the browser
+   */
+  public native String getName();
+
+  /*
+   * Version of the browser currently in use.
+   *
+   * @return version of the browser
+   */
   public native String getVersion();    
-    
+
+  /*
+   * Returns the average navigation speed for the currently bound
+   * NavigationInfo node in meters/second.
+   *
+   * @return current speed in metres/second
+   */
   public native float getCurrentSpeed();
-    
+
+  /*
+   * Return the current frame rate in frames/second.
+   *
+   * @return current frame rate
+   */
   public native float getCurrentFrameRate();
     
+  /*
+   * Return the URL for the root of the currently loaded world.
+   *
+   * @return root URL of the currently loaded world
+   */
   public native String getWorldURL();
+
+  /*
+   * Replaces the current world with the world represented by the 
+   * passed nodes.
+   *
+   * @param nodes New world
+   */
   public native void replaceWorld(BaseNode[] nodes);
-    
-  public native BaseNode[] createVrmlFromString(String vrmlSyntax);
-  //     throws InvalidVRMLSyntaxException;
-    
-  public native void createVrmlFromURL(String[] url, BaseNode node, String event);
-  //     throws InvalidVRMLSyntaxException;
-    
+
+  /*
+   * This method parses a string consisting of VRML statements, establishes
+   * any PROTO and EXTERNPROTO declarations and routes, and returns an MFNode
+   * value containing the set of nodes in those statements.
+   *
+   * @param vrmlSyntax VRML statements
+   * @return Array of nodes representing the given VRML statements.
+   * @throws InvalidVRMLSyntaxException If invalid syntax in string
+   */   
+  public native BaseNode[] createVrmlFromString(String vrmlSyntax)
+    throws InvalidVRMLSyntaxException;
+
+  /*
+   * Instructs the browser to load a VRML scene description from the 
+   * given URL or URLs. After the scene is loaded, the given event
+   * is sent to the passed node.
+   *
+   * @param url array of URLs
+   * @param node node to send event to
+   * @param event event to send to node
+   * @throws InvalidVRMLSyntaxException If URL contain invalid syntax
+   */
+  public native void createVrmlFromURL(String[] url, BaseNode node, 
+				       String event)
+    throws InvalidVRMLSyntaxException;
+
+  /*
+   * Add a route between the given event names for the given nodes.
+   *
+   * @param fromNode Node where route originates
+   * @param fromEvenOut Event Out where route originates
+   * @param toNode Node where route ends
+   * @param toEventIn Event In where route ends
+   */
   public native void addRoute(BaseNode fromNode, String fromEventOut,
 			      BaseNode toNode, String toEventIn);
-    
+
+  /*
+   * Delete a route between the given event names for the given nodes.
+   *
+   * @param fromNode Node where route originates
+   * @param fromEvenOut Event Out where route originates
+   * @param toNode Node where route ends
+   * @param toEventIn Event In where route ends
+   */
   public native void deleteRoute(BaseNode fromNode, String fromEventOut,
 				 BaseNode toNode, String toEventIn);
-    
-  public native void loadURL(String[] url, String[] parameter);
-  //     throws InvalidVRMLSyntaxException;
-    
+
+  /*
+   * Loads the first recognized URL from the specified url field with 
+   * the passed parameters.
+   *
+   * @param url array of URLs
+   * @param parameter parameters to load URL with
+   * @exception InvalidVrmlSyntaxException If URL has invalid VRML syntax
+   */    
+  public native void loadURL(String[] url, String[] parameter)
+    throws InvalidVRMLSyntaxException;
+
+  /*
+   * Sets the passed string as the current description
+   *
+   * @param description New description
+   */
   public native void setDescription(String description);
 }
