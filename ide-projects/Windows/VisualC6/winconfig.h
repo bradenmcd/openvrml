543bf661a448ce83699162ce1bdec2b3294e1c5c
Braden McDaniel <braden@endoframe.com>
2001-10-13 04:53:25 +0000

    M_PI, etc. nolonger used.

--- ide-projects/Windows/VisualC6/winconfig.h
+++ ide-projects/Windows/VisualC6/winconfig.h
@@ -22,26 +22,6 @@ static const char SLASH = '/';
 
 static const char COLON = ':';
 
-/* Make sure that PI and friends are defined.  This is needed under 
-   platforms that are not directly BSD derived (even under GNU libc
-   this is not included by default).
-*/
-#ifndef M_PI
-#define M_PI            3.14159265358979323846  /* pi */
-#endif
-
-#ifndef M_PI_2
-#define M_PI_2          1.57079632679489661923  /* pi/2 */
-#endif
-
-#ifndef M_PI_4
-#define M_PI_4          0.78539816339744830962  /* pi/4 */
-#endif
-
-#ifndef M_1_PI
-#define M_1_PI          0.31830988618379067154  /* 1/pi */
-#endif
-
 
 #define HAVE_LIBJPEG
 #define HAVE_LIBPNG
