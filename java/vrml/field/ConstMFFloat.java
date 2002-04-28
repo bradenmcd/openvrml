a5d2a50b4a66387ab87fb5efc64a210d92e822f2
tflynn <tflynn@62bbe328-0443-0410-860d-e6d70dd76ebd>
2002-04-28 05:59:46 +0000

    Add additional comments.

--- java/vrml/field/ConstMFFloat.java
+++ java/vrml/field/ConstMFFloat.java
@@ -22,17 +22,50 @@ package vrml.field;
 
 import vrml.ConstMField;
 
+/**
+ * Represents a read-only VRML MFFloat field in Java.
+ */
 public class ConstMFFloat extends ConstMField
 {
   public native int getSize();
 
+  /**
+   * Construct a new MFFloat field in OpenVRML using the given params.
+   *
+   * @param size Number of SFFloat elements passed in.
+   * @param values List of SFFloat values.
+   */
   private native void CreateObject(int size, float values[]);
 
+  /**
+   * Construct a read-only MFFloat field.
+   *
+   * @param size Number of SFFloat elements passed in.
+   * @param values Array of SFFloat values.
+   */
   public ConstMFFloat(int size, float values[]) { CreateObject(size, values); }
+
+  /**
+   * Construct a read-only MFFloat field.
+   *
+   * @param values An array of SFFloat values.
+   */
   public ConstMFFloat(float values[])  { CreateObject(values.length, values); }
-	
+
+  /**
+   * Retrieves the value of an MFFloat field.
+   *
+   * @param Array of float values to be returned.
+   */
   public native void getValue(float values[]);
-	
+
+  /**
+   * Retrieves a specific SFFloat element in an MFFloat and
+   * returns it as a float.
+   *
+   * @param index Position of desired SFFloat
+   * @return Value of SFFloat at index.
+   */	
   public native float get1Value(int index);
 	
   public native String toString();
