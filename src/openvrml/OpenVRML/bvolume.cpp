622bc30a33767e6789680cefe9d867784dcf8f22
Braden McDaniel <braden@endoframe.com>
2003-03-21 19:43:05 +0000

    #include <limits> instead of <float.h>.

--- src/openvrml/OpenVRML/bvolume.cpp
+++ src/openvrml/OpenVRML/bvolume.cpp
@@ -18,7 +18,7 @@
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
 
-# include <float.h>
+# include <limits>
 # include "private.h"
 # include "bvolume.h"
 # include "field.h"
