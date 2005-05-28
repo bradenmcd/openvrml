5f4cb5dac1fc141c927a2faf3e035feb849e35ae
Braden McDaniel <braden@endoframe.com>
2005-05-28 06:37:03 +0000

    Removed extraneous GTK+ compiler flags and #includes.

--- mozilla-plugin/src/openvrml.cpp
+++ mozilla-plugin/src/openvrml.cpp
@@ -40,11 +40,6 @@
 # include <nsIDOMWindow.h>
 # include <dom/nsIDOMWindowInternal.h>
 # include <dom/nsIDOMLocation.h>
-# if defined MOZ_X11
-#   include <gdk/gdkx.h>
-# else
-#   error Unsupported toolkit.
-# endif
 # include "openvrml.h"
 
 namespace {
