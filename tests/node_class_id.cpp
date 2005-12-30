a60de30c3bc5eed35b48931f06ca158e898ad5d4
Braden McDaniel <braden@endoframe.com>
2005-12-30 05:13:08 +0000

    Added node_class_id test.

---
+++ tests/node_class_id.cpp
@@ -0,0 +1,42 @@
+# include <boost/test/unit_test.hpp>
+# include <openvrml/browser.h>
+
+using namespace std;
+using namespace openvrml;
+
+void construct_from_urn()
+{
+    node_class_id id("urn:foo:bar");
+}
+
+void construct_from_url()
+{
+    node_class_id id("http://example.com");
+}
+
+void construct_from_url_with_fragment_id()
+{
+    node_class_id id("http://example.com#Foo");
+}
+
+void construct_from_relative_url()
+{
+    BOOST_CHECK_THROW(node_class_id id("../foo/bar"), std::invalid_argument);
+}
+
+void construct_from_arbitrary_string()
+{
+    BOOST_CHECK_THROW(node_class_id id("string"), std::invalid_argument);
+}
+
+boost::unit_test::test_suite * init_unit_test_suite(int, char * [])
+{
+    using boost::unit_test::test_suite;
+    test_suite * const suite = BOOST_TEST_SUITE("node_class_id");
+    suite->add(BOOST_TEST_CASE(&construct_from_urn));
+    suite->add(BOOST_TEST_CASE(&construct_from_url));
+    suite->add(BOOST_TEST_CASE(&construct_from_url_with_fragment_id));
+    suite->add(BOOST_TEST_CASE(&construct_from_relative_url));
+    suite->add(BOOST_TEST_CASE(&construct_from_arbitrary_string));
+    return suite;
+}
