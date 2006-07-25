867b9f537d335fadc1d5a9d98bdb84b13cb46651
Braden McDaniel <braden@endoframe.com>
2006-07-25 15:32:22 +0000

    Changes to tests for parity with the change from node_class to node_metatype.

--- tests/node_metatype_id.cpp
+++ tests/node_metatype_id.cpp
@@ -6,38 +6,39 @@ using namespace openvrml;
 
 void construct_from_urn()
 {
-    node_class_id id("urn:foo:bar");
+    node_metatype_id id("urn:foo:bar");
 }
 
 void construct_from_url()
 {
-    node_class_id id("http://example.com");
+    node_metatype_id id("http://example.com");
 }
 
 void construct_from_url_with_fragment_id()
 {
-    node_class_id id("http://example.com#Foo");
+    node_metatype_id id("http://example.com#Foo");
 }
 
 void construct_nested_proto_id()
 {
-    node_class_id id("http://example.com#Foo#Bar");
+    node_metatype_id id("http://example.com#Foo#Bar");
 }
 
 void construct_from_relative_url()
 {
-    BOOST_CHECK_THROW(node_class_id id("../foo/bar"), std::invalid_argument);
+    BOOST_CHECK_THROW(node_metatype_id id("../foo/bar"),
+                      std::invalid_argument);
 }
 
 void construct_from_arbitrary_string()
 {
-    BOOST_CHECK_THROW(node_class_id id("string"), std::invalid_argument);
+    BOOST_CHECK_THROW(node_metatype_id id("string"), std::invalid_argument);
 }
 
 boost::unit_test::test_suite * init_unit_test_suite(int, char * [])
 {
     using boost::unit_test::test_suite;
-    test_suite * const suite = BOOST_TEST_SUITE("node_class_id");
+    test_suite * const suite = BOOST_TEST_SUITE("node_metatype_id");
     suite->add(BOOST_TEST_CASE(&construct_from_urn));
     suite->add(BOOST_TEST_CASE(&construct_from_url));
     suite->add(BOOST_TEST_CASE(&construct_from_url_with_fragment_id));
