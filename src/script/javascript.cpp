a5b0cb908e8f5d32d5e662067e998457e48205cf
Braden McDaniel <braden@endoframe.com>
2011-01-22 07:37:47 +0000

    SpiderMonkey included in the forthcoming XULRunner 2.0 changes the return value of JSVAL_TO_DOUBLE to a double (instead of a pointer).

--- src/script/javascript.cpp
+++ src/script/javascript.cpp
@@ -82,6 +82,15 @@ namespace {
 # endif
     }
 
+    OPENVRML_LOCAL jsdouble jsval_to_double(const jsval val)
+    {
+# if OPENVRML_JSVAL_TO_DOUBLE_RETURNS_POINTER
+        return *JSVAL_TO_DOUBLE(val);
+# else
+        return JSVAL_TO_DOUBLE(val);
+# endif
+    }
+
     class SFNode;
     class MFNode;
 
@@ -5856,7 +5865,7 @@ namespace {
         out << '[';
         for (JsvalArray::size_type i = 0; i < mfdata->array.size(); ++i) {
             assert(JSVAL_IS_DOUBLE(mfdata->array[i]));
-            out << *JSVAL_TO_DOUBLE(mfdata->array[i]);
+            out << jsval_to_double(mfdata->array[i]);
             if ((i + 1) < mfdata->array.size()) { out << ", "; }
         }
         out << ']';
@@ -6235,7 +6244,7 @@ namespace {
         std::vector<float> temp = mffloat->value();
         for (MField::JsvalArray::size_type i = 0; i < mfdata->array.size(); ++i) {
             assert(JSVAL_IS_DOUBLE(mfdata->array[i]));
-            temp[i] = float(*JSVAL_TO_DOUBLE(mfdata->array[i]));
+            temp[i] = float(jsval_to_double(mfdata->array[i]));
         }
         mffloat->value(temp);
         return mffloat;
@@ -6307,7 +6316,7 @@ namespace {
         std::vector<double> temp = mfdouble->value();
         for (MField::JsvalArray::size_type i = 0; i < mfdata->array.size(); ++i) {
             assert(JSVAL_IS_DOUBLE(mfdata->array[i]));
-            temp[i] = *JSVAL_TO_DOUBLE(mfdata->array[i]);
+            temp[i] = jsval_to_double(mfdata->array[i]);
         }
         mfdouble->value(temp);
         return mfdouble;
@@ -7284,7 +7293,7 @@ namespace {
         std::vector<double> temp = mftime->value();
         for (MField::JsvalArray::size_type i = 0; i < mfdata->array.size(); ++i) {
             assert(JSVAL_IS_DOUBLE(mfdata->array[i]));
-            temp[i] = *JSVAL_TO_DOUBLE(mfdata->array[i]);
+            temp[i] = jsval_to_double(mfdata->array[i]);
         }
         mftime->value(temp);
         return mftime;
